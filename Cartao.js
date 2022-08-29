c0c9b24e5f0f53f23aa1c375b2d8d8d29214bb6d
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2022-08-29 14:54:40 -0300

    Inclusão do grupo de estilos para o componente cartao

--- Cartao.js
+++ Cartao.js
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
-import { Image, Text, View } from "react-native";
-
+import { Image, Text, View, StyleSheet } from "react-native";
+const area = StyleSheet.create({
+    direcao: { flexDirection: 'row' }
+})
+const imagem = StyleSheet.create({
+    afastamento: { padding: 10 },
+    tamanho: { width: 100, height: 100 }
+})
+const texto = StyleSheet.create({
+    afastamento: { top: 30 },
+    cor: { color: '#89CFF0' }
+})
 export default class Cartao extends Component {
     render() {
         let url = this.props.url
         return (
-            <View style={{ flexDirection: 'row' }}>
-                <View style={{ padding: 10 }}>
-                    <Image source={{ uri: url }} style={{ width: 100, height: 100 }} ></Image>
+            <View style={area.direcao}>
+                <View style={imagem.afastamento}>
+                    <Image source={{ uri: url }} style={imagem.tamanho} ></Image>
                 </View>
-                <View style={{top:30}}>
-                    <Text style={{ color: '#89CFF0' }}>
+                <View style={texto.afastamento}>
+                    <Text style={texto.cor}>
                         {this.props.nome}
                     </Text>
-                    <Text style={{ color: '#89CFF0' }}>
+                    <Text style={texto.cor}>
                         {this.props.disciplina}
                     </Text>
                 </View>
