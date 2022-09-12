4123161a0b2fb86ac7d8a30dd707f7795c1faeae
Gerson Penha <gerson.penha@fatec.sp.gov.br>
2022-09-12 13:43:28 -0300

    Inclusão de um componente com tamanho e posição fixa

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
-import Cartao from './Cartao';
+import React from "react"
+import { Component } from "react"
+import { View, Text } from "react-native"
 
-export default class App extends Component {
-  urls = [
-    'https://fatecsjc-prd.azurewebsites.net/images/team/docentes/reinaldo_gen_ichiro_arakaki.jpg',
-    'http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4164443U3',
-    'https://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4767313D7',
-    'https://fatecsjc-prd.azurewebsites.net/images/team/docentes/gerson_da_penha_neto.jpg'
-  ]
+class App extends Component {
   render() {
     return (
-      <>
-        <Cartao url={this.urls[0]} nome='Reinaldo Arakake' disciplina='Matemática para Computação' />
-        <Cartao url={this.urls[1]} nome='Cláudio Lima' disciplina='Engenharia de Software' />
-        <Cartao url={this.urls[2]} nome='Fernando Masanori' disciplina='Algoritmos' />
-        <Cartao url={this.urls[3]} nome='Gerson Penha' disciplina='Programação Orientada à Objetos' />
-      </>
+      <View style={{backgroundColor:'black'}}>
+        <View style={{ width: 100, height: 50, backgroundColor: 'red' }}>
+          <Text style={{ color: 'white' }}>Elemento</Text>
+        </View>
+      </View>
     )
   }
 }
+export default App
 
